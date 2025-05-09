b8f104f9d2c7074babf8e34e14c48c42a3b1b8f5
Huw Jones <huw@pexip.com>
2025-05-09 15:24:03 +0000

    Prepare Pexip packaging.

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+Bug-Database: https://github.com/Stranger6667/hypothesis-openapi/issues
+Bug-Submit: https://github.com/Stranger6667/hypothesis-openapi/issues/new
+Changelog: https://github.com/Stranger6667/hypothesis-openapi/blob/main/CHANGES
+Documentation: https://github.com/Stranger6667/hypothesis-openapi/wiki
+Repository-Browse: https://github.com/Stranger6667/hypothesis-openapi
+Repository: https://github.com/Stranger6667/hypothesis-openapi.git
